8e229a3996098673848d41beb359fb84328e81eb
goya <773949603@qq.com>
2025-07-24 22:31:55 +0800

    refactor: 重构

---
+++ module/module-jpa/src/main/java/com/ysmjjsy/goya/module/jpa/configuration/JpaAutoConfiguration.java
@@ -0,0 +1,17 @@
+package com.ysmjjsy.goya.module.jpa.configuration;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+
+/**
+ * <p></p>
+ *
+ * @author goya
+ * @since 2025/7/22 23:39
+ */
+@Slf4j
+@AutoConfiguration
+@RequiredArgsConstructor
+public class JpaAutoConfiguration {
+}
